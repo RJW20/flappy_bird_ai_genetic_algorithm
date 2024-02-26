e84c8982d9db5fd93c409b84e38075d0b5b5748c
RJW20 <robertjwatts84@outlook.com>
2024-02-26 21:22:13 +0000

    Implement the template methods

--- flappy_bird_ai/player.py
+++ flappy_bird_ai/player.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import numpy as np
 
 from flappy_bird_app import Bird
@@ -11,15 +13,34 @@ class Player(Bird, BasePlayer):
         self.score: int = 0
         self.fitness: int = 0
         self.best_score: int = 0
+        self.vision: np.ndarray
 
     def look(self) -> None:
-        """Update the attributes used as input to the Genome."""
-        pass
+        """Set the bird's vision.
+        
+        Can see: distance to the pipe, own vertical velocity, vertical distance 
+        to the end of the top of the pipe, vertical distance to the end of the 
+        bottom of the pipe.
+        """
+
+        first_pipe = self.pipes.items[0]
+        self.vision = np.array[first_pipe.position - self.x, 
+                               self.velocity,
+                               self.position - first_pipe.height, 
+                               first_pipe.bottom_height - self.position]
 
     def think(self) -> int:
         """Feed the input into the Genome and turn the output into a valid move."""
-        pass 
+        
+        genome_output = self.genome.propagate(self.vision)
+        return round(genome_output[0], 1)      #jump if > 0.5 (sigmoid activation)
 
     def empty_clone(self) -> BasePlayer:
         """Return a new instance of self's class without a genome."""
-        pass
+
+        clone = deepcopy(self)
+        clone.fitness = 0
+        clone.best_score = 0
+        clone.genome = None
+
+        return clone
